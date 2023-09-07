3da7a05ee87129f61a2019ed02d41fe568847950
Vikas V Pattar <81294766+vikasvpattar@users.noreply.github.com>
2023-09-07 17:17:41 +0530

    added Path Modules

---
+++ Path Modules/index.js
@@ -0,0 +1,7 @@
+const path = require("path");
+const a = path.basename("index.js");
+const b = path.dirname("index.js");
+const c = path.extname("index.js");
+console.log(a);
+console.log(b);
+console.log(c);
