38ae2e30860692bc4cc283a4cbcc0c049edc9358
Vikas V Pattar <81294766+vikasvpattar@users.noreply.github.com>
2023-09-08 21:52:17 +0530

    HTML file rendering and file downloading API

--- Express/index.js
+++ Express/index.js
@@ -1,5 +1,15 @@
 const express = require("express");
 const app = express();
+const path = require("path");
+// finding path of file
+const pathDir = path.join(__dirname, "public");
+console.log(pathDir);
+
+app.use(express.static(pathDir));
+// sending HTML file
+app.get("/home", (req, res) => {
+  res.sendFile(`${pathDir}/home.html`);
+});
 
 // --Get  API using Express
 app.get("/", (req, res) => {
@@ -8,6 +18,20 @@ app.get("/", (req, res) => {
 
 app.get("/about", (req, res) => {
   res.send("This is about page");
+});
+
+// sending JSON file
+app.get("/json", (req, res) => {
+  res.send({
+    nme: "vikas",
+    age: 23,
+    email: "vikasvpattar18@gmail.com",
+  });
+});
+
+// file downloading
+app.get("/download", (req, res) => {
+  res.download(`${pathDir}/index.html`);
 });
 
 app.listen(8000, () => {
