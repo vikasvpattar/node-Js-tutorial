4fa940206154f6af564123bd0399d9f8b611c51c
Vikas V Pattar <81294766+vikasvpattar@users.noreply.github.com>
2023-09-08 19:30:54 +0530

    JSON  Data  is added

--- server/index.js
+++ server/index.js
@@ -1,9 +1,15 @@
 const http = require("http");
+const fs = require("fs");
+
 const server = http.createServer((req, res) => {
+  const jsonData = fs.readFileSync("api.json", "utf-8");
+  const objData = JSON.parse(jsonData);
   if (req.url === "/") {
     res.end("My first server using nodejs");
   } else if (req.url === "/download") {
     res.end("Download page");
+  } else if (req.url === "/about") {
+    res.end(objData[0].name);
   } else {
     res.end("404: Page not found");
   }
---
+++ server/jsontest.js
@@ -0,0 +1,8 @@
+const obj = {
+  name: "vikas",
+  age: 23,
+  address: "Bangalore",
+};
+console.log(obj);
+const jsonData = JSON.stringify(obj);
+console.log(jsonData);
